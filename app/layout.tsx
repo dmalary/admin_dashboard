a093f806089a7ff57788a6c0b77a38355df9b53f
madhat5 <davidmalary@gmail.com>
2024-03-12 18:32:17 -0400

    feat: add auth functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -15,6 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Navbar />
       <body className={inter.className}>{children}</body>
     </html>
   );

97af00035edf82094e1025f17b9c59639fb1915a
madhat5 <davidmalary@gmail.com>
2024-03-24 20:47:13 -0400

    feat: display data

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -50,7 +50,26 @@ export default function Analytics() {
       <Grid numItemsSm={2} numItemsLg={3} className="gap-6">
         {data.map((datum) => (
           <Card key={datum.category}>
-            card
+            <Title>{datum.category}</Title>
+            <Flex
+              justifyContent="start"
+              alignItems="baseline"
+              className="space-x-2"
+            >
+              <Metric>{datum.stat}</Metric>
+              <Text>Total views</Text>
+            </Flex>
+            <Flex className="mt-6">
+              <Text>Pages</Text>
+              <Text className="text-right">Views</Text>
+            </Flex>
+            <BarList
+              data={datum.data}
+              valueFormatter={(number: number) =>
+                Intl.NumberFormat("us").format(number).toString()
+              }
+              className="mt-2"
+            />
           </Card>
         ))}
       </Grid >
